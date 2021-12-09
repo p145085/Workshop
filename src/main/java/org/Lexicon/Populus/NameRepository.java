20358de30c769a4014da8b8cc18b8ddf60b49f20
emila <emil.a.johansson@gmail.com>
2021-12-09 11:54:38 +0100

    Finished the next part of the workshop.

--- src/main/java/org/Lexicon/Populus/NameRepository.java
+++ src/main/java/org/Lexicon/Populus/NameRepository.java
@@ -40,18 +40,55 @@ public class NameRepository {
     }
 
     public static void main(String[] args) {
-        System.out.println(getSize());  // We get the length of our array.
-        setNames(names);                // We replace every element of our array with "ReplacedName".
-        System.out.println(names[0]);   // We test to see if we did replace the names.
-        System.out.println(names[1]);   // We test to see if we did replace the names.
-        clear();                        // We set every element to be an empty String.
-        System.out.println(names[0]);   // We test to see if we did empty the elements.
-        System.out.println(names[1]);   // We test to see if we did empty the elements.
-        String[] anArray = findAll();   // We set a new array to get the copied array because of testing purposes.
-        for(int i = 0; i < anArray.length; i++){    // We loop through every element in the new array to see if we are getting every element in it.
-            System.out.println(anArray[i]);
-        }
+//        System.out.println(getSize());  // We get the length of our array.
+//        setNames(names);                // We replace every element of our array with "ReplacedName".
+//        System.out.println(names[0]);   // We test to see if we did replace the names.
+//        System.out.println(names[1]);   // We test to see if we did replace the names.
+//        clear();                        // We set every element to be an empty String.
+//        System.out.println(names[0]);   // We test to see if we did empty the elements.
+//        System.out.println(names[1]);   // We test to see if we did empty the elements.
+//        String[] anArray = findAll();   // We set a new array to get the copied array because of testing purposes.
+//        for(int i = 0; i < anArray.length; i++){    // We loop through every element in the new array to see if we are getting every element in it.
+//            System.out.println(anArray[i]);
+//        }
+
+//        System.out.println(find("foo"));
+        System.out.println(add("ABC"));
+        System.out.println(Arrays.toString(names));
+
     }
 
+    public static String find(final String fullName) {
+//    Returns name if found and null if not found.
+        for(int i = 0; i < names.length; i++){
+            if(fullName == names[i]){
+                return names[i];
+            }
+        }
+        return null;
+    }
+
+    public static boolean add(final String fullName) {
+//    Should add a new name to the array. Returns true when name was added and false when the array contains
+//    the name.
+
+        for(int i = 0; i < names.length; i++){
+            if (fullName == names[i]){
+                return false;
+            }
+        }
+        String[] copyOfArray = Arrays.copyOf(names, names.length + 1);
+        copyOfArray[copyOfArray.length - 1] = fullName;
+        System.out.println(copyOfArray[copyOfArray.length - 1]);
+        names = copyOfArray;
+        return true;
+    }
+
+
+//    public static String[] addNameToArray final String[] source , String name )
+//    String[]
+//            newArray = Arrays. copyOf source , source length + newArray
+//    newArray length 1] = name ; //Adding the name to last index of newArray
+//    return newArray
 
 }
